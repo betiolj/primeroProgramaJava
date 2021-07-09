992563e94a31127580bde90f8df11a9b613c2be9
klebe <klebe@DESKTOP-F89IPVP>
2021-07-09 14:04:38 -0300

    modulo 13 - aula 5 agora

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -7,19 +7,12 @@ public class ArrayVetor {
 	public static void main(String[] args) {
 		/* Array pode ser de todos os tipos de dados e objetos */
 
-		String posicoes = JOptionPane.showInputDialog("Quantas posições o Array deve ter?");
-
-		/* Array sempre deve ter a quantidade de posições definidas */
-		double[] notas = new double[Integer.parseInt(posicoes)];
-
-		for (int pos = 0; pos < notas.length; pos++) {
-			String valor = JOptionPane.showInputDialog("Qual o valor da nota = " + pos);
-			notas[pos] = Double.valueOf(valor);
+		String[] valores = 	{"jose", "90","java","contado@com.br"};
+		
+		for(int pos = 0; pos < valores.length; pos++) {
+			System.out.println("Valor na posição " +pos+ " = "+ valores[pos]);
 		}
-
-		for (int pos = 0; pos < notas.length; pos++) {
-			System.out.println("Notas " + (pos + 1) + " = " + notas[pos]);
-		}
+		
 	}
 
 }
