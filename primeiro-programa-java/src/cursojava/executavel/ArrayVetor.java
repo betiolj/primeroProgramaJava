1754c17ea873354d90ae4805f6678338d056db0c
klebe <klebe@DESKTOP-F89IPVP>
2021-07-10 15:16:33 -0300

    modulo 13 - Aula 8 - descobrindo a maior nota do aluno

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -6,35 +6,50 @@ import cursojava.classes.Disciplina;
 public class ArrayVetor {
 
 	public static void main(String[] args) {
-		
-		double[] notas = {10.0, 9.7, 8.6, 6.9};
-		double[] notas2 = {9.0, 7.7, 8.9, 7.9};
-		
+
+		double[] notas = { 9.0, 9.7, 10.0, 6.9 };
+		double[] notas2 = { 9.0, 7.7, 9.9, 7.9 };
+
 		Aluno aluno = new Aluno();
 		aluno.setNome("jose");
 		aluno.setNomeEscola("escola tal");
-		
+
 		Disciplina disciplina = new Disciplina();
 		disciplina.setDisciplina("java");
 		disciplina.setNota(notas);
-		
+
 		aluno.getDisciplinas().add(disciplina);
-		
+
 		Disciplina disciplina2 = new Disciplina();
 		disciplina2.setDisciplina("banco");
 		disciplina2.setNota(notas2);
-		
+
 		aluno.getDisciplinas().add(disciplina2);
-		
-		
+
 		System.out.println("Nome do aluno = " + aluno.getNome() + " inscrito no curso: " + aluno.getNomeEscola());
 		System.out.println("------Discilplinas do aluno--------");
-		for(Disciplina disc : aluno.getDisciplinas()) {
+
+		for (Disciplina disc : aluno.getDisciplinas()) {
+
 			System.out.println("Disciplina: " + disc.getDisciplina());
 			System.out.println("Notas da disciplina: ");
-			for(int pos = 0; pos < disc.getNota().length; pos++) {
+			
+			double notaMax = 0.0;
+			
+			for (int pos = 0; pos < disc.getNota().length; pos++) {
 				System.out.println("Nota " + pos + " = " + disc.getNota()[pos]);
+				
+				if(pos == 0) {
+					notaMax = disc.getNota()[pos];
+				}else {
+					if(disc.getNota()[pos] > notaMax) {
+						notaMax = disc.getNota()[pos];
+					}
+				}
+				
 			}
+			
+			System.out.println("A maior nota da Disciplina " +disc.getDisciplina() + " = "+notaMax);
 		}
 	}
 
