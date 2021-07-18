a20dbef7d6dabc93493c22875d40f4006d876900
klebe <klebe@DESKTOP-F89IPVP>
2021-07-18 20:22:54 -0300

    modulo 14 - aula 7 - conhecendo o GridBagConstraints gerenciador de posicionamento e layout

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -1,10 +1,25 @@
 package cursojava.thread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TelaTimeThread extends JDialog{ /*jDialog SWING tela*/
+	
+	private JPanel jPanel = new JPanel(new GridBagLayout()); /*Painel de componentes*/
+	
+	private JLabel descricaoHora = new JLabel("Time Thread 1");
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
+	private JTextField mostraTempo2 = new JTextField();
+	
 
 	/*Construtor*/
 	public TelaTimeThread() {
@@ -14,7 +29,26 @@ public class TelaTimeThread extends JDialog{ /*jDialog SWING tela*/
 		setResizable(false);
 		/*primeira parte concluida*/
 		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints(); /*controlador de posicionamento de componentes*/
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
 		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
+		
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo, gridBagConstraints);
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
+		
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo2, gridBagConstraints);
+		
+		add(jPanel, BorderLayout.WEST);
 		/*sempre sera o ultimo comando*/
 		setVisible(true); /*torna a tela visivel*/
 	}
