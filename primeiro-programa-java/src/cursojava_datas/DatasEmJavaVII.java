217ee08d25710a68b75277aaa04b61e8a07264d9
betiolj <42118980+betiolj@users.noreply.github.com>
2021-10-05 22:48:09 -0300

    Modulo 15 - aula 10 - Dia, meses, anos e semanas

--- primeiro-programa-java/src/cursojava_datas/DatasEmJavaVII.java
+++ primeiro-programa-java/src/cursojava_datas/DatasEmJavaVII.java
@@ -6,23 +6,15 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-
-public class DatasEmJavaVI {
+public class DatasEmJavaVII {
 	
 	public static void main(String[] args) throws ParseException{
-	
-		/*Nova API de data a partir do Java **/
 		
 		LocalDate dataAtudal = LocalDate.now();
 		System.out.println("Data atual: " + dataAtudal.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-		
-		LocalTime horaAtudal = LocalTime.now();
-		System.out.println("Hora atual: " + horaAtudal.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
-		
-		LocalDateTime dataHoraAtudal = LocalDateTime.now();
-		System.out.println("Data e Hora atual: " + dataHoraAtudal.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
-		
-		
+		System.out.println("Dia da semana: " + dataAtudal.getDayOfWeek().name());
+		System.out.println("Dia do mes: " + dataAtudal.getDayOfMonth());
+		System.out.println("Dia do ano: " + dataAtudal.getYear());
 	}
 
 }
