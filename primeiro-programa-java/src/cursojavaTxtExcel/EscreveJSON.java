56b19e0c7c7204e39faa7cd2b0868cb3f93c19f3
klebe <klebe@DESKTOP-F89IPVP>
2021-11-03 15:41:57 -0300

    modulo 16 - aula 16.10 - Lendo arquivo JSON e montando Lista em java

--- primeiro-programa-java/src/cursojavaTxtExcel/EscreveJSON.java
+++ primeiro-programa-java/src/cursojavaTxtExcel/EscreveJSON.java
@@ -1,6 +1,7 @@
 package cursojavaTxtExcel;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,6 +9,9 @@ import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class EscreveJSON {
 
@@ -40,6 +44,22 @@ public class EscreveJSON {
 		fileWriter.write(jsonUser);
 		fileWriter.flush();
 		fileWriter.close();
+		
+		//******************************* LENDO O ARQUIVO JSON************************
+		
+		FileReader fileReader = new FileReader("C:\\Users\\klebe\\git\\primeroProgramaJava\\primeiro-programa-java\\src\\cursojavaTxtExcel\\filjson.json");
+		
+		JsonArray jsonArray = (JsonArray) JsonParser.parseReader(fileReader);
+		
+		List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+		
+		for (JsonElement jsonElement : jsonArray) {
+			
+			Usuario usuario = new Gson().fromJson(jsonElement, Usuario.class);
+			listaUsuarios.add(usuario);
+		}
+		
+		System.out.println("leitura do arquivo JSON: " + listaUsuarios);
 	}
 
 }
--- primeiro-programa-java/src/cursojavaTxtExcel/Usuario.java
+++ primeiro-programa-java/src/cursojavaTxtExcel/Usuario.java
@@ -38,6 +38,10 @@ public class Usuario implements Serializable{
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+	@Override
+	public String toString() {
+		return "Usuario [login=" + login + ", senha=" + senha + ", cpf=" + cpf + ", nome=" + nome + "]";
+	}
 	
 
 
